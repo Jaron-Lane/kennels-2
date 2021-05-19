3582017d1a9b96153113cd7da0fc087aae01d922
Jaron-Lane <jaronwohlwend@icloud.com>
2021-05-19 12:41:32 -0500

    Setting up"

---
+++ src/components/animal/AnimalCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Animal.css";
+
+export const AnimalCard = () => (
+    <section className="animal">
+        <h3 className="animal__name">This is a name of animal</h3>
+        <div className="animal__breed">This is a breed of animal</div>
+    </section>
+)
