5cc4ea710ac973d1f6177db6532bd9639890fd3d
Jaron-Lane <jaronwohlwend@icloud.com>
2021-05-26 15:39:31 -0500

    Integrated count-up button

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -4,6 +4,7 @@ import { CustomerCard } from "./customer/CustomerCard";
 import { EmployeeCard } from "./employee/EmployeeCard";
 import "./Kennel.css"
 import { LocationCard } from "./location/LocationCard";
+import { PropsAndState } from "./PropsAndState";
 
 export const Kennel = () => (
     <>
@@ -14,6 +15,8 @@ export const Kennel = () => (
             <div>Visit Us at the Nashville North Location</div>
             <div>500 Puppy Way</div>
         </address>
+
+        <PropsAndState yourName="Jaron"/>
 
         <h2>Animals</h2>
         <section className="animals">
