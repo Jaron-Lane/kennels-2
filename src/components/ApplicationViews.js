c94ad1d021a1435be72098ba3ac5e47f36248bb0
Jaron-Lane <jaronwohlwend@icloud.com>
2021-06-07 15:58:40 -0500

    Building AnimalList component with routing

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,4 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { Home } from './Home';
-import { AnimalList } from './animal/'
+import { AnimalList } from './animal/';
+
+export const ApplicationViews = () => {
+    return (
+        <>
+            <Route exact path="/">
+                <Home />
+            </Route>
+
+            <Route path="/animals">
+                <AnimalList />
+            </Route>
+        </>
+    )
+}
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import { AnimalCard } from './AnimalCard';
+import "./Animal.css";
+
+export const AnimalList = () => (
+    <>
+        <h2>Animals</h2>
+        <section className="animals">
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+        </section>
+    </>
+)
